9f4d8a5306be76d7970b791a306f230b9fed92ee
Lucas Rocha <lucasregodarocha@outlook.com>
2020-08-07 21:24:28 -0300

    preliminary version of box created.

--- src/components/ContainerBoxesText.js
+++ src/components/ContainerBoxesText.js
@@ -24,57 +24,56 @@ export default class ContainerBoxesText extends Component {
     }
     componentDidMount  = (e) => {
         let drag = false;
-        let actualX = 0;
-        let actualY = 0;
+        // let actualX = 10000;
+        // let actualY = 10000;
         const containerBox = document.getElementById('containerBox');
         containerBox.addEventListener('mousedown', (e) => {
             this.setState({startX:e.clientX});
             this.setState({startY:e.clientY});
+            const test = document.createElement('div');
+            test.setAttribute('id','test');    
             const testBox = document.createElement('div');
             testBox.setAttribute('class','box-in-test');
             testBox.setAttribute('id','boxTest');    
             containerBox.appendChild(testBox);
+            containerBox.appendChild(test);
             const containerTest = document.getElementById('boxTest');
             containerTest.style.left = this.state.startX + 'px'; // starting positions
             containerTest.style.top = this.state.startY + 'px'; // starting positions
-            actualX = this.state.startX;
-            actualY = this.state.startY;
+            containerTest.style.width = '0.01px';
+            containerTest.style.height = '0.01px';
             drag = true;
         });
         containerBox.addEventListener('mousemove' , (e) => {
-            if(drag){
-                const boxTestContainer = document.getElementById('boxTest');
-                const rectBox = boxTestContainer.getBoundingClientRect();
-                if(e.clientX > this.state.startX){
-                    boxTestContainer.style.width = rectBox.width - (actualX - e.clientX) + 'px';
-                    actualX = e.clientX;
-                }
-                if(e.clientY > this.state.startY){
-                    boxTestContainer.style.height = rectBox.height - (actualY - e.clientY) + 'px';
-                    actualY = e.clientY;
-                }
-                if(e.clientX < this.state.startX){
-                    boxTestContainer.style.width = rectBox.width + (e.clientX -actualX) + 'px';
-                    actualX = e.clientX;
-                    this.setState({startX:e.clientX});
-                    boxTestContainer.style.left = this.state.startX + 'px'; // starting positions
-
-                }
-                if(e.clientY < this.state.startY){
-                    boxTestContainer.style.height = rectBox.height + (e.clientY -actualY) + 'px';
-                    actualY = e.clientY;
-                    this.setState({startY:e.clientY});
-                    boxTestContainer.style.top = this.state.startY + 'px'; // starting positions
-                }
-                
-                console.log(rectBox);
+            if(!drag) return ;
+            const boxTestContainer = document.getElementById('boxTest');
+            if(e.clientX > this.state.startX){
+                boxTestContainer.style.width = (e.clientX - this.state.startX) + 'px';
             }
+
+            if(e.clientY > this.state.startY){
+                boxTestContainer.style.height = (e.clientY - this.state.startY) + 'px';
+                
+            }
+            if(e.clientX === this.state.startX){
+                boxTestContainer.style.width ='0.01px';
+            }
+            if(e.clientY === this.state.startY){
+                boxTestContainer.style.height ='0.01px';
+            }
+            if(e.clientX < this.state.startX){
+                boxTestContainer.style.width = (this.state.startX - e.clientX) + 'px';
+                boxTestContainer.style.left = (e.clientX) + 'px'; // starting positions
+            }
+
+            if(e.clientY < this.state.startY){
+                boxTestContainer.style.height = (this.state.startY - e.clientY)+ 'px';
+                boxTestContainer.style.top = (e.clientY) + 'px'; // starting positions
+            }
+            
         });
         containerBox.addEventListener('mouseup' , (e) => {
-            drag = false;
-            const box = document.getElementById('boxTest');
-            box.style.height='0px';
-            box.style.width='0px';   
+            drag = false; 
         });
     }
     
